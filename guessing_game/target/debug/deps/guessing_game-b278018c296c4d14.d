5300972c0755508ef0ae796ed339ad96cb794e9c
Tom <tom@thirdpartytrust.com>
2020-04-21 20:08:48 -0300

    remove compiles from guessing_game

--- guessing_game/target/debug/deps/guessing_game-b278018c296c4d14.d
+++
@@ -1,5 +0,0 @@
-/Users/roark/projects/rust-practice/guessing_game/target/debug/deps/guessing_game-b278018c296c4d14: src/main.rs
-
-/Users/roark/projects/rust-practice/guessing_game/target/debug/deps/guessing_game-b278018c296c4d14.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/roark/projects/rust-practice/guessing_game/target/debug/guessing_game: /Users/roark/projects/rust-practice/guessing_game/src/main.rs
