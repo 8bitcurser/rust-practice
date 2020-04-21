0780b8f2cf5d5041a0a459d7e8bb804ff025a8ef
Tom <tom@thirdpartytrust.com>
2020-04-21 20:07:55 -0300

    remove compiles

--- hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d
+++
@@ -1,5 +0,0 @@
-/Users/roark/projects/rust-practice/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae: src/main.rs
-
-/Users/roark/projects/rust-practice/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/roark/projects/rust-practice/hello_cargo/target/debug/hello_cargo: /Users/roark/projects/rust-practice/hello_cargo/src/main.rs
