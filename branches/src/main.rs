0c055fd8c3cdd6ff755af4cf9647277b9e52d069
Tom <tom@thirdpartytrust.com>
2020-04-24 19:56:44 -0300

    Finish chapter 3 and create projects to excercise

---
+++ branches/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number = 5;
+    if number < 5 {
+        println!("The cond was true");
+    } else {
+        println!("The cond was false");
+    }
+    
+}
---
+++ loops/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Take of in...");
+    for number in (1..6). rev(){
+        println!("{}..", number);
+    }
+    println!("LAUNCH!");
+}
