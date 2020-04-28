2de8f3e9f35dcfaaa509e477c0aa6deb0d4c5256
Tom <tom@thirdpartytrust.com>
2020-04-27 22:32:51 -0300

    Add twelve days of christmas and fibonacci exercise

--- twelve_days/src/main.rs
+++ twelve_days/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let nums = [
+        ["first", "a partridge in a pear tree"],
+        ["second", "Two turtle doves"],
+        ["third", "three French hens"],
+        ["fourth", "Four calling birds"],
+        ["fifth", "five gold rings"],
+        ["sixth", "six geese a laying"],
+        ["seventh", "Seven swans a swimming"],
+        ["eighth", "eight maids a milking"],
+        ["ninth", "nine ladies dancing"],
+        ["tenth", "Ten lords a leaping"],
+        ["eleventh", "eleven pipers piping"],
+        ["twelfth", "Twelve drummers drumming"] 
+    ];
+    for day in 0..12 {
+        println!(
+            "On the {} day of Christmas my true love gave to me",
+            nums[day][0]
+        );
+       for verse in (0..day+1).rev(){
+        if day != 0 && verse == 0{
+            print!("and ");
+        }
+            println!("{}", nums[verse][1]);
+       }
+       println!("=========================");
+    }
+
 }
