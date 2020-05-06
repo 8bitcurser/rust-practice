dc2c9e84a25e4eb88b5dfe842df42d79aa00e320
Tom <tom@thirdpartytrust.com>
2020-05-06 00:27:33 -0300

    Got to packaging babyy

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,29 @@
+
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(_) => 25,
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)));
+    println!("{}", value_in_cents(Coin::Dime));
 }
