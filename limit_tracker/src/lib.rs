ab0483d49ee926386d63e1745a9e16a55abae165
Tom G <tomowl@pm.me>
2020-07-14 00:37:32 -0300

    Started working around the interior mutability project

---
+++ limit_tracker/src/lib.rs
@@ -0,0 +1,29 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize
+}
+
+impl<'a, T> LimitTracker<'a, T>
+    where T: Messenger {
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max
+        }
+
+    }    
+
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
