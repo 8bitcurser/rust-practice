872534d15a281230662efdc06982620b7ad38347
Tom G <tomowl@pm.me>
2020-07-24 01:14:36 -0300

    Add mroe comments

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,4 +1,5 @@
 pub struct Post {
+    // dyn stands for dynamically check for X trait
     state: Option<Box<dyn State>>,
     content: String
 }
@@ -6,6 +7,8 @@ pub struct Post {
 impl Post {
     pub fn new() -> Post {
         Post {
+            // a new post has a state that is Some box that points to a type
+            // of Draft, state is private so we cant change it from the API.
             state: Some(Box::new(Draft {})),
             content: String::new()
         }
@@ -18,6 +21,9 @@ impl Post {
     pub fn content(&self) -> &str {
         // as_ref on the Option because we want the value inside the Option
         // rather than ownership over it
+        // &self would be the post parameter when the state is Published
+        // this is possible thanks to deref coercion that will track all the
+        // way down to the value
         self.state.as_ref().unwrap().content(&self)
     }
 
@@ -28,16 +34,20 @@ impl Post {
         // state. Clearer is like this with take we grab the state and put a
         // None temporarily while we obtain the new state but to do so we need
         // to use the previous state which now we control (s.request_review())
-        // and set it to the field.
+        // and set it to the field. If we would do self.state = 
+        // self.state.request_review(); we wouldnt be owning the state, we
+        // would be having the Some which has no method request_review.
         if let Some(s) = self.state.take() {
             self.state = Some(s.request_review())
         }
     }
 
     pub fn approve(&mut self) {
-        // we do the same, we TAKE the inner state, replace it temporarily with 
-        // none (we claim its ownership) and we use the previous state to 
-        // obtain a new one.
+        // we do the same, we TAKE the inner state (value inside Option),
+        // replace it temporarily with none (we claim its ownership) and
+        // we use the previous state to 
+        // obtain a new one. The none is necessary because Rust does not allow
+        // us to have unpopulated fields in structs.
         if let Some(s) = self.state.take() {
             self.state = Some(s.approve())
         }
